b22a5ae353bfb1ebc6a565ea36c6a6e4ce6ffaa1
Cool Acid <coolacid217@gmail.com>
2014-02-10 00:08:49 -0600

    Defining animate function

--- js/ecobici.js
+++ js/ecobici.js
@@ -4,7 +4,17 @@ function map_init() {
     zoom: 13
   };
   var map = new google.maps.Map(document.getElementById("theMap"), mapOptions);
+  return map;
 }
+
+function animate(start, end, map) {
+  var markerStart = new google.maps.Marker({position:start, map:map});
+  var markerEnd = new google.maps.Marker({position:end, map:map});
+}
+
 $(function(){
-  map_init();
+  var map = map_init();
+  var start = new google.maps.LatLng(19.41544,-99.164856);
+  var end = new google.maps.LatLng(19.423535,-99.1446);
+  animate(start, end, map);
 });
