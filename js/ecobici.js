73051e78fb4fdfd719f0391614a0edf13584be3e
Cool Acid <coolacid217@gmail.com>
2014-02-09 23:48:28 -0600

    Centering in ecobici zone

--- js/ecobici.js
+++ js/ecobici.js
@@ -1,7 +1,7 @@
 function map_init() {
   var mapOptions = {
-    center: new google.maps.LatLng(-34.397, 150.644),
-    zoom: 8
+    center: new google.maps.LatLng(19.422887,-99.167418),
+    zoom: 13
   };
   var map = new google.maps.Map(document.getElementById("theMap"), mapOptions);
 }
